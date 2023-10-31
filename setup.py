ce5fa80c6af6e9b2db1774051728ec0566525437
[Deepak Kumar] <[dkprajapati90133@gmail.com]>
2023-10-31 20:57:35 +0530

    Data Ingestion Completed

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = "-e ."
-
-def get_requirements(file_path:str) -> List[str]:
-    '''This function is going to return the list of all requirements'''
-
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements = [req.replace('\n',"") for req in requirements]
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
-        return requirements
+    
+    return requirements
 
 setup(
-    name='Score',
-    version='0.0.1',
-    author='Deepak',
-    author_email='dkprajapati90133@gmail.com',
-    packages=find_packages(),
-    install_requires= get_requirements('requirements.txt')
-    
-    )
+name='mlproject',
+version='0.0.1',
+author='Deepak',
+author_email='dkprajapati90133@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
